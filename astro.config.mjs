4362c470f86cfa50d61f4a8427459b56470e9253
Elyshor <ulemar91@googlemail.com>
2026-07-05 11:58:50 +0200

    Add SEO basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://elyshor.de',
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => ![
+        'https://elyshor.de/datenschutz/',
+        'https://elyshor.de/impressum/',
+      ].includes(page),
+    }),
+  ]
 });
